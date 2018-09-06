ccd449b25041ecc4d3674f4b9b5a468b56e3efae
Ryan Alexander <10535959+RyanAlexander91@users.noreply.github.com>
2018-09-06 22:42:27 +0100

    Version 0.1 Alpha
    Added basic implentation of application logic.

--- ProgressBar/ProgressBar.UserInterface.WPF/MainWindow.xaml.cs
+++ ProgressBar/ProgressBar.UserInterface.WPF/MainWindow.xaml.cs
@@ -20,9 +20,54 @@ namespace ProgressBar.UserInterface.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Constructor
+
+        // Constructor
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        #endregion
+
+        #region UserInterface Click Events
+        /// <summary>
+        /// Updates progress bar based on user input
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Update_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateProgressBar();
+
+        }
+
+        private void MenuItem_Reset_Click(object sender, RoutedEventArgs e)
+        {
+            ResetProgressBar();
+        }
+
+        #endregion
+
+        #region Application Logic Code
+
+        private void UpdateProgressBar()
+        {
+            int userInput = int.Parse(TextBox_Entry.Text);
+            ProgressBar_Main.Value = userInput;
+
+            if (ProgressBar_Main.Value < ProgressBar_Main.Minimum || ProgressBar_Main.Value > ProgressBar_Main.Maximum)
+            {
+                MessageBox.Show("Please enter a valid number between 0 and 100", "Invalid Input", MessageBoxButton.OK);
+            }
+        }
+
+        private void ResetProgressBar()
+        {
+            ProgressBar_Main.Value = 0;
+            TextBox_Entry.Text = string.Empty;
+        }
+
+        #endregion
     }
 }
