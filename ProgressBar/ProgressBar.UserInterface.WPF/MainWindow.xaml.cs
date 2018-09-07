617cf3047d064bd4a79eac45179030032c7d9634
Ryan Alexander <10535959+RyanAlexander91@users.noreply.github.com>
2018-09-07 02:28:28 +0100

    Version 0 .1 Beta 1 * Added try catch block in application logic. * Implemented MessageBox functionality for all MenuItems *Added Application Icon

--- ProgressBar/ProgressBar.UserInterface.WPF/MainWindow.xaml.cs
+++ ProgressBar/ProgressBar.UserInterface.WPF/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ProgressBar.UserInterface.WPF
 {
@@ -47,19 +36,74 @@ namespace ProgressBar.UserInterface.WPF
             ResetProgressBar();
         }
 
+        private void MenuItem_Acknowledgements_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayAcknowledgementsDialog();
+        }
+
+        private void MenuItem_About_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayAboutDialog();
+        }
+
+        private void MenuItem_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
         #endregion
 
         #region Application Logic Code
 
         private void UpdateProgressBar()
         {
-            int userInput = int.Parse(TextBox_Entry.Text);
-            ProgressBar_Main.Value = userInput;
+            /** Regex Information
+             *
+             *  ^ means the start of the string
+             *  [] means the character set
+             *  +  means one time or more
+             *  $  means end of the string
+             *  ^  and $ are needed to validate all of the string string, 
+             *  not just part of the string 
+             **/
 
-            if (ProgressBar_Main.Value < ProgressBar_Main.Minimum || ProgressBar_Main.Value > ProgressBar_Main.Maximum)
+            Regex reg = new Regex("^[a-zA-Z]+$");
+
+            // Check to see if TextBox_Entry.Text contains letters
+            if(reg.IsMatch(TextBox_Entry.Text) == false)
             {
-                MessageBox.Show("Please enter a valid number between 0 and 100", "Invalid Input", MessageBoxButton.OK);
+                try
+                {
+                    // Parse string input into an int
+                    var userInput = int.Parse(TextBox_Entry.Text);
+
+                    // Validate input
+                    if(userInput < ProgressBar_Main.Minimum || userInput > ProgressBar_Main.Maximum)
+                    {
+                        DisplayInvalidInputMessage();
+                    }
+                    else
+                    {
+                        ProgressBar_Main.Value = userInput;
+                    }
+                    
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
             }
+            else
+            {
+                DisplayInvalidInputMessage();
+            }
+
+
+        }
+
+        private static void DisplayInvalidInputMessage()
+        {
+            MessageBox.Show("Please enter a valid number between 0 and 100", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Error);
         }
 
         private void ResetProgressBar()
@@ -68,6 +112,18 @@ namespace ProgressBar.UserInterface.WPF
             TextBox_Entry.Text = string.Empty;
         }
 
+        private static void DisplayAcknowledgementsDialog()
+        {
+            MessageBox.Show("Progress Bar application logo used as per the CC0 Creative Commons license.", "Acknowledgements", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private static void DisplayAboutDialog()
+        {
+            MessageBox.Show("Application designed and developed by Ryan Alexander. 2018", "About", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
         #endregion
+
+
     }
 }
